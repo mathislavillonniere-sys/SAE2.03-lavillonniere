4e012c0e436627180c377d31a7a379bc963666d4
Lavillonniere Mathis <lavillonniere7@mmi-dev.unilim.fr>
2026-04-22 15:21:42 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "lavillonniere7");
+define("DBLOGIN", "lavillonniere7");
+define("DBPWD", "lavillonniere7");
 
 
 function getAllMovies(){
