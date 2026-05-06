d1da54643892fdb4deb877839fc468dd21d2355f
mathislavillonniere <mathis.lavillonniere@gmail.com>
2026-05-06 16:51:55 +0200

    ité surprise

--- iterationsurprise/app/component/iteration3/MovieDetail/script.js
+++ iterationsurprise/app/component/iteration3/MovieDetail/script.js
@@ -7,7 +7,7 @@ let MovieDetail = {};
 
 MovieDetail.format = function (film, isFav) {
   let html = template;
-  // ON AJOUTE LE REMPLACEMENT DE L'ID ICI
+
   html = html.replaceAll("{{id}}", film.id);
   html = html.replaceAll("{{name}}", film.name);
   html = html.replace("{{image}}", "../../server/images/" + film.image);
@@ -18,14 +18,13 @@ MovieDetail.format = function (film, isFav) {
   html = html.replace("{{min_age}}", film.min_age);
   html = html.replace("{{trailer}}", film.trailer);
 
-  // SI DÉJÀ EN FAVORI : On affiche le bouton "Retirer"
+  // Boutons Favoris stylisés (btn-add et btn-remove doivent être dans ton CSS global)
   if (isFav == 1) {
     html = html.replace(
       "{{favoris}}",
-      `<button class="btn-remove" onclick="C.handlerDeleteFavoris(${film.id})">Retirer des favoris</button>`,
+      `<button class="btn-remove" onclick="C.handlerDeleteFavoris(${film.id})">Retirer</button>`,
     );
   } else {
-    // SINON : On affiche le bouton "Ajouter"
     html = html.replace(
       "{{favoris}}",
       `<button class="btn-add" onclick="C.handlerAddFavoris(${film.id})">Ajouter aux favoris</button>`,
